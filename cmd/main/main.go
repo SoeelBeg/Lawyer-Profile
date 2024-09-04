4fb67634db40b27d64d4389765d77f52ca079215
SoeelBeg <khansohil50858@gmail.com>
2024-09-03 23:34:27 -0700

    Move code to handler package for Vercel deployment

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,6 @@
-package main
+package handler
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/soeel/lawyer-profile/pkg/config"
@@ -9,17 +8,16 @@ import (
 	"github.com/soeel/lawyer-profile/pkg/routes"
 )
 
-func main() {
+// Handler function for Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router := routes.RegisterRoutes()
+	router.ServeHTTP(w, r)
+}
+
+func init() {
 	// Connect to the database
 	config.ConnectDatabase()
 
 	// Migrate the lawyer model
 	models.MigrateLawyer(config.GetDB())
-
-	// Register routes
-	router := routes.RegisterRoutes()
-
-	// Start the server
-	log.Println("Server is running on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", router))
 }
