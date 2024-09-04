2a005658cde31f8ff5996b6d869e6b4ea6142c73
SoeelBeg <khansohil50858@gmail.com>
2024-09-04 01:59:34 -0700

    Fix syntax errors and update database connection code

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,8 +11,20 @@ import (
 
 var db *gorm.DB
 
+// ConnectDatabase initializes the database connection
 func ConnectDatabase() {
-	dsn := "admin:soeel123@tcp(127.0.0.1:3306)/soeeldb?charset=utf8mb4&parseTime=True&loc=Local"
+	// Fetching environment variables for database connection
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Construct the DSN (Data Source Name)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// Open the database connection using GORM
 	var err error
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -18,6 +32,7 @@ func ConnectDatabase() {
 	}
 }
 
+// GetDB returns the database instance
 func GetDB() *gorm.DB {
 	return db
 }
