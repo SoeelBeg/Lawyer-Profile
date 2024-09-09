617d7fbef16e96e872aeafc88056e3c66ffe28fa
SoeelBeg <khansohil50858@gmail.com>
2024-09-09 00:48:26 -0700

    Update database connection configuration

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -11,18 +11,18 @@ import (
 var db *gorm.DB
 
 func ConnectDatabase() {
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		log.Fatal("DATABASE_URL is not set in the environment")
-	}
+    dsn := os.Getenv("DATABASE_URL")
+    if dsn == "" {
+        log.Fatal("DATABASE_URL is not set in the environment")
+    }
 
-	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to the database!", err)
-	}
+    var err error
+    db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to the database!", err)
+    }
 }
 
 func GetDB() *gorm.DB {
-	return db
+    return db
 }
